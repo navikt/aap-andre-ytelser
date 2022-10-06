69f801521dc5d6d0babaa6b842185650ee9bd215
Richard Borge <richard.borge@nav.no>
2022-10-06 09:47:58 +0200

    Init gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+val aapLibVersion = "3.1.5"
+val ktorVersion = "2.1.2"
+
+plugins {
+    id("com.github.johnrengelman.shadow")
+    application
+}
+
+application {
+    mainClass.set("no.nav.aap.AppKt")
+}
+
+dependencies {
+    implementation("com.github.navikt.aap-libs:ktor-utils:$aapLibVersion")
+    implementation("com.github.navikt.aap-libs:ktor-client-auth:$aapLibVersion")
+    implementation("com.github.navikt.aap-libs:kafka:$aapLibVersion")
+    testImplementation("com.github.navikt.aap-libs:kafka-test:$aapLibVersion")
+
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-auth:$ktorVersion")
+
+    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.4")
+
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:1.4.1")
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.2")
+
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+}
