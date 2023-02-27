455370345c74464edc3b72730416f07cf6d54db9
Niro Karunakaran <niro.karunakaran@nav.no>
2023-02-27 15:19:04 +0100

    Oppdatert dependencies
    Co-authored-by: Robin T <robin.tordly@nav.no>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
-val aapLibVersion = "3.4.7"
-val ktorVersion = "2.1.2"
+val aapLibVersion = "3.6.8"
+val ktorVersion = "2.2.3"
 
 plugins {
-    id("com.github.johnrengelman.shadow")
-    application
+    kotlin("jvm") version "1.8.10"
+    id("io.ktor.plugin") version "2.2.3"
 }
 
 application {
@@ -16,7 +16,7 @@ dependencies {
     implementation("com.github.navikt.aap-libs:kafka:$aapLibVersion")
     testImplementation("com.github.navikt.aap-libs:kafka-test:$aapLibVersion")
 
-    implementation("com.github.navikt:aap-vedtak:1.0.82")
+    implementation("com.github.navikt.aap-vedtak:kafka-dto:1.1.1")
 
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
@@ -39,4 +39,19 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "19"
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
 }
